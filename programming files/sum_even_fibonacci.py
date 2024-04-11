1670f7ea4500b8bd2dbc9cfd94ead9f1543c866e
mclark41 <143416514+mclark41@users.noreply.github.com>
2024-04-11 17:01:34 -0400

    Update sum_even_fibonacci.py
    Broke

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,7 +1,7 @@
 a, b = 0, 1  # Define starting values
 sum_even = 0
 while b < 4000000: # Sets upper limit
-    if b % 2 == 0: # Uses the fact that all even numbers are divisible by 2 to check it is even
+    if b // 2 == 0: # Uses the fact that all even numbers are divisible by 2 to check it is even
         sum_even += b # Adds b to the sum
     a, b = b, a+b # Updates the values
 
