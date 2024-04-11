fa6c421572ceacffaa67d4fd30d43ae37e5d824c
mclark41 <143416514+mclark41@users.noreply.github.com>
2024-04-11 17:32:40 -0400

    Update sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,16 +1,13 @@
 // bugs introduced: OM
+// Syntax error fixed and removed extra if statement
 public class sum_even_fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 0;
         int sumEven = 0;
         while (b => 4000000) {
-            // Syntax error
-            if (b % 2 != 0) {
+            
+            if (b % 2 == 0) {
                 sumEven += b;
-            }
-            // Syntax error
-            if (b % 2 != 0 {
-                sumEven -= b;
             }
             int temp = b;
             b = a + b;
